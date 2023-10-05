c3e77d0eec97181dcc4e77d5582a6690632f7940
TeeMeeLehin <samemerald8@gmail.com>
2023-10-05 15:23:13 +0100

    archive tgz

---
+++ 1-pack_web_static.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Fabfile to compress web static folder"""
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """Create a .tgz archive from the contents of the web_static folder."""
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+
+    local("mkdir -p versions")
+
+    archive_name = f"web_static_{timestamp}.tgz"
+
+    zipped = local(f"tar -cvzf versions/{archive_name} web_static")
+
+    if zipped.succeeded:
+        return f"versions/{archive_name}"
+    else:
+        return None
