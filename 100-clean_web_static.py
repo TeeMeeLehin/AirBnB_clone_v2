e75c2bd05d3c431a1f5ff638fa7a13e1cd4d569f
TeeMeeLehin <samemerald8@gmail.com>
2023-10-08 21:39:16 +0100

    del old archives

---
+++ 100-clean_web_static.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Fabfile  that deletes out-of-date archives"""
+from fabric.api import env, run, local, task
+from datetime import datetime
+from os import listdir
+from os.path import isfile, join
+
+env.hosts = ['54.160.126.192', '54.159.22.198']
+env.user = 'ubuntu'
+env.key_filename = '~/.ssh/id_rsa'
+
+
+def do_clean(number=0):
+    """Delete out-of-date archives from versions and releases folders."""
+
+    number = int(number)
+    archive_dir = 'versions'
+    LA = [f for f in listdir(archive_dir) if isfile(join(archive_dir, f))]
+
+    LA.sort(reverse=True)
+
+    archives_to_delete = LA[number:]
+    for archive in archives_to_delete:
+        local("rm -f {}/{}".format(archive_dir, archive))
+
+    releases_dir = '/data/web_static/releases'
+    releases = run("ls -1 {}".format(releases_dir)).split('\n')
+
+    releases_to_delete = releases[number:]
+    for release in releases_to_delete:
+        if release != 'test':
+            run("rm -rf {}/{}".format(releases_dir, release))
+
+    return True
