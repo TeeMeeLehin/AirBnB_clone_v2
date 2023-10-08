63333c81e3825c294259effbb7ec949bad5ec197
TeeMeeLehin <samemerald8@gmail.com>
2023-10-08 21:07:45 +0100

    deploy fab py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -19,10 +19,13 @@ def do_deploy(archive_path):
         archive_filename = archive_path.split('/')[-1]
         archive_name_no_ext = archive_filename.split('.')[0]
         release_path = f"/data/web_static/releases/{archive_name_no_ext}"
+        run(f"rm -rf {release_path}")
         run(f"mkdir -p {release_path}")
         run(f"tar -xzf /tmp/{archive_filename} -C {release_path}")
 
         run(f"rm /tmp/{archive_filename}")
+        run(f"mv {release_path}/web_static/* {release_path}/")
+        run(f"rm -rf {release_path}/web_static")
 
         current_link = '/data/web_static/current'
         run(f"rm -rf {current_link}")
